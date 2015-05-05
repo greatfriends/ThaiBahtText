5ec4c53d0315c24ebaa4ec521931cb8bac045876
Suthep Saengwirotjanapat <suthep@gfbd.co.th>
2015-05-05 23:20:21 +0700

    add nuspec

--- GFDN.ThaiBahtText/Properties/AssemblyInfo.cs
+++ GFDN.ThaiBahtText/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ThaiBahtText")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generate money amount text in Thai language for example: from 121.50 will be 'หนึ่งร้อยยี่สิบเอ็ดบาทห้าสิบสตางค์' ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GreatFriends.Biz")]
 [assembly: AssemblyProduct("ThaiBahtText")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5307cf59-e282-4f9a-b4c8-7e57fb8aaadc")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
