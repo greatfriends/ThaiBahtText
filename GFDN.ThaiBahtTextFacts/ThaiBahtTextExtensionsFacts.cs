f74c02e62ddad7299a92b7825eee1532b128df06
Suthep Saengwirotjanapat <suthep@gfbd.co.th>
2015-05-16 21:21:36 +0700

    change target framework to .NET 4.0

--- GFDN.ThaiBahtTextFacts/ThaiBahtTextExtensionsFacts.cs
+++ GFDN.ThaiBahtTextFacts/ThaiBahtTextExtensionsFacts.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using GreatFriends.ThaiBahtText;
+using System;
 using Xunit;
-using GreatFriends.ThaiBahtText;
 
 namespace GreatFriends.ThaiBahtTextFacts {
   public class ThaiBahtTextExtensionsFacts {
