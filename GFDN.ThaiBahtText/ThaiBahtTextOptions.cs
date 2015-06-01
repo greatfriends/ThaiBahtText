b40bfaed281d4124d1c07457067fa69c952bb36d
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-06-01 07:24:47 +0700

    Fix #27

--- GFDN.ThaiBahtText/ThaiBahtTextOptions.cs
+++ GFDN.ThaiBahtText/ThaiBahtTextOptions.cs
@@ -9,15 +9,20 @@ namespace GreatFriends.ThaiBahtText {
     public UsesEt Mode { get; set; }
     public Unit Unit { get; set; }
     public bool AppendBahtOnly { get; set; }
+    public int DecimalPlaces { get; set; }
 
     public ThaiBahtTextOptions()
       : this(mode: UsesEt.TensOnly,
              unit: ThaiBahtText.Unit.Baht,
-             appendBahtOnly: true) {
+             appendBahtOnly: true,
+             decimalPlaces: 2) {
       //
     }
 
-    public ThaiBahtTextOptions(UsesEt mode, Unit unit, bool appendBahtOnly) {
+    public ThaiBahtTextOptions(UsesEt mode = UsesEt.TensOnly,
+                               Unit unit = Unit.Baht,
+                               bool appendBahtOnly = true,
+                               int decimalPlaces = 2) {
       this.Mode = mode;
       this.Unit = unit;
       this.AppendBahtOnly = appendBahtOnly;
