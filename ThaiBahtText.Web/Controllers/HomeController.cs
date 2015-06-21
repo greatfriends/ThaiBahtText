cbc82c564e9f7052f7971f9a057bfbfe2c444619
Suthep <suthep@gfbd.co.th>
2015-06-21 21:53:06 +0700

    add portal web site

---
+++ ThaiBahtText.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using GreatFriends.ThaiBahtText;
+
+namespace ThaiBahtText.Web.Controllers {
+  public class HomeController : Controller {
+
+    public ActionResult Index() {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult ToThaiBahtText(decimal? amount) {
+      if (amount == null) {
+        amount = 0m;
+      }
+      var result = new{
+        AmountText = amount.Value.ToString("n2"),
+        ThaiBahtText = amount.ThaiBahtText()
+      };
+      return Json(result);
+    } 
+
+  }
+}
