0e262c506cea3509fad42b30bf871f4c24fd1784
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-07-27 10:26:34 +0700

    pex generated file

---
+++ ui-test/conf.js
@@ -0,0 +1,10 @@
+// conf.js
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+      browserName: 'chrome'
+    }]
+}
---
+++ ui-test/spec.js
@@ -0,0 +1,19 @@
+// spec.js
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+ 
+  it('should add one and two', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+    element(by.model('first')).sendKeys(1);
+    element(by.model('second')).sendKeys(2);
+
+    element(by.id('gobutton')).click();
+
+    expect(element(by.binding('latest')).getText()).
+        toEqual('3'); // This is wrong!
+  });
+});
