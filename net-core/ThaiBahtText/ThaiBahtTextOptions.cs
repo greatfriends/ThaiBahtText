37b547bbfaa2fd5dda6a33fab400a847bc7f63d4
Nattapong Nunpan <aixasz@hotmail.com>
2017-08-20 14:50:14 +0700

    re create in .net standard 2.0

---
+++ net-core/ThaiBahtText/ThaiBahtTextOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace ThaiBahtText
+{
+  public class ThaiBahtTextOptions
+  {
+
+    public UsesEt UsesEt { get; set; }
+    public Unit Unit { get; set; }
+    public bool AppendBahtOnly { get; set; }
+    public int DecimalPlaces { get; set; }
+
+    public ThaiBahtTextOptions()
+      : this(usesEt: UsesEt.TensOnly,
+             unit: Unit.Baht,
+             decimalPlaces: 2,
+             appendBahtOnly: true)
+    {
+    }
+
+    public ThaiBahtTextOptions(UsesEt usesEt = UsesEt.TensOnly, Unit unit = Unit.Baht, int decimalPlaces = 2, bool appendBahtOnly = true)
+    {
+      UsesEt = usesEt;
+      Unit = unit;
+      DecimalPlaces = decimalPlaces;
+      AppendBahtOnly = appendBahtOnly;
+    }
+  }
+}
+
